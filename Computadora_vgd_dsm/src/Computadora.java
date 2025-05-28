113ea67ab142a1c9fe3b883374e502252e3939cb
Vixo-vDev <20243ds046@utez.edu.mx>
2025-05-28 07:54:30 -0600

    Agregando getters and setter

--- Computadora_vgd_dsm/src/Computadora.java
+++ Computadora_vgd_dsm/src/Computadora.java
@@ -1,5 +1,41 @@
 public class Computadora {
-    
+
     private String marca, modelo;
     private boolean encendida;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public boolean isEncendida() {
+        return encendida;
+    }
+
+    public void setEncendida(boolean encendida) {
+        this.encendida = encendida;
+    }
+
+    public void encender(){
+        if (encendida == false){
+            encendida = true;
+        }
+    }
+
+    public void apagar(){
+        if (encendida == true){
+            encendida = false;
+        }
+    }
 }
