81ad51105599d012f9842a383197246c8ee48b70
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-05-30 09:02:04 -0600

    Realizando código en el main, usando getters and setters

--- U1_TI_05_IMC/src/App.java
+++ U1_TI_05_IMC/src/App.java
@@ -1,5 +1,33 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-      
+
+        Scanner read = new Scanner(System.in);
+        double imc;
+
+        Persona persona1 = new Persona();
+
+        System.out.println("Ingresa tu nomnbre:");
+        persona1.setNombre(read.nextLine());
+
+
+        System.out.println("Ingresa tu altura:");
+        persona1.setAltura(read.nextDouble());
+
+        System.out.println("Ingresa tu peso:");
+        persona1.setPeso(read.nextDouble());
+
+        imc = persona1.calcularIMC();
+
+        persona1.clasificarIMC(imc);
+
+        System.out.println("Nombre: "+ persona1.getNombre());
+        System.out.println("Alutra en metros: "+ persona1.getAltura());
+        System.out.println("Peso: "+ persona1.getPeso()+"kg");
+        System.out.println("Tienes: "+persona1.clasificarIMC(imc));
+
+
     }
 }
--- U1_TI_05_IMC/src/Persona.java
+++ U1_TI_05_IMC/src/Persona.java
@@ -22,6 +22,31 @@ public class Persona {
         this.peso = peso;
     }
 
+    public double calcularIMC(){
+        return  peso /(altura*altura);
+    }
+
+    public String clasificarIMC(double imc){
+        String clasificacion ="";
+
+        if(imc < 18.5){
+            clasificacion = "Bajo de peso";
+        }
+
+        else if(imc >= 18.5 && imc <=24.9){
+            clasificacion="Peso normal";
+
+        }else if (imc >= 25.0 && imc <= 29.9) {
+            clasificacion="Sobrepeso";
+        }
+        else{
+            clasificacion = "Obesidad";
+        }
+        
+
+        return clasificacion;
+    }
+
     
     
 }
